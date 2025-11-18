6e85a37516d2342a9c021e640e1ad48502a8783c
Suit sangita <suitsangitasahoo@gmail.com>
2025-11-18 09:08:32 +0530

    Added project files

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -27,7 +27,13 @@ const About = () => {
                 I am a software developer skilled in <b>Core Java, Web Development (HTML, CSS, JavaScript)</b>, and backend frameworks like <b>Servlets, JSP, and Spring Boot</b>. 
                 I also have strong knowledge of <b>databases such as Oracle and MySQL</b>, along with experience in <b>REST API development, MVC architecture, and OOPs concepts</b>.
               </p>
+              <a
+  href="/Sangita-Resume.pdf"
+  download="Sangita-Resume.pdf"
+  className="flex items-center gap-2  text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 transition"
+>
               <button className="cv-btn">Download CV <FaCloudDownloadAlt /></button>
+              </a>
             </div>
             <div className="skills">
               <div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -43,10 +43,14 @@ const Navbar = () => {
         </ul>
 
         
-        <button className="hidden md:flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 transition">
-          <MdOutlineCloudDownload size={22} />
-          Download CV
-        </button>
+        <a
+  href="/Sangita-Resume.pdf"
+  download="Sangita-Resume.pdf"
+  className="flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 transition"
+>
+  <MdOutlineCloudDownload size={22} />
+  Download CV
+</a>
 
         
         <button
